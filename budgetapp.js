a4553c96738a77227be11cb8e22efd69006a264e
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-09-04 00:20:09 -0500

    Started overall functionality of budget calculator, nothing crazy just building the foundation.

--- budgetapp.js
+++ budgetapp.js
@@ -1,3 +1,41 @@
 /**
  * Created by daniel on 9/3/17.
  */
+
+var budgetController = (function () {
+
+    var x = 23;
+
+    var add = function (a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function (b) {
+            return add(b)
+        }
+    }
+
+})();
+
+
+
+var UIController = (function () {
+
+    // Some code
+
+})();
+
+
+
+var Controller = (function (budgetCtrl, UICtrl) {
+
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublicTest: function () {
+            console.log(z);
+        }
+    }
+
+})(budgetController, UIController);
