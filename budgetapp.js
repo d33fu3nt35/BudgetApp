1957e1662813dd61d29ffe98f906b9ec02b2b79d
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-09-04 01:11:00 -0500

    Layed out overall plan for background funtionality to do list in code. Added some code so when a user clicks the check button it fires off an event or if a user hits return on there keyboard it fires off that same event without duplicating code.

--- budgetapp.js
+++ budgetapp.js
@@ -22,10 +22,34 @@ var UIController = (function () {
 // GLOBAL APPLICATION CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', function () {
+    var ctrlAddItem = function () {
 
-        console.log('Button Was Clicked!')
+        // 1. Get the field input data
 
-    })
+        // 2. Add the item to the budget
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+        console.log('Testing, Testing.');
+
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
+    });
 
 })(budgetController, UIController);
+
+
+
+
