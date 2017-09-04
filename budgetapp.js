60e5bdf7eff0fc81b581616917ea3e5015488433
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-09-04 01:51:02 -0500

    .

--- budgetapp.js
+++ budgetapp.js
@@ -5,6 +5,28 @@
 // BUDGET CONTROLLER
 var budgetController = (function () {
 
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItem: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
 
 })();
 
@@ -52,7 +74,6 @@ var controller = (function (budgetCtrl, UICtrl) {
             }
         });
     };
-
 
 
     var ctrlAddItem = function () {
