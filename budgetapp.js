c24efba9eddda9ff579b3923685908aae09b4a6e
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-09-05 01:39:50 -0500

    Input fields now clear out when you click return key or click on submit button & it returns focus on description input for easier input

--- budgetapp.js
+++ budgetapp.js
@@ -112,6 +112,21 @@ var UIController = (function () {
 
         },
 
+        clearFields: function () {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            var fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function (current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -153,6 +168,10 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 3. Add the item to the UI
 
         UICtrl.addListItem(newItem, input.type);
+
+        // Clear The Input Fields
+
+        UICtrl.clearFields();
 
         // 4. Calculate the budget
 
