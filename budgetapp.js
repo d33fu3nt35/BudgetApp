a03334c31511f7c9a3aa0a174dcac80543fafcff
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-09-05 02:32:16 -0500

    Budget calculations are now displayed on user interface and all numbers on UI restart to zero upon refresh.

--- budgetapp.js
+++ budgetapp.js
@@ -116,7 +116,11 @@ var UIController = (function () {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
 
     return {
@@ -171,6 +175,22 @@ var UIController = (function () {
 
         },
 
+        displayBudget: function (obj) {
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+            if (obj.percentage > 0) {
+
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -204,7 +224,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         var budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
 
     };
 
@@ -242,6 +262,12 @@ var controller = (function (budgetCtrl, UICtrl) {
     return {
         init: function () {
             console.log('Application has started.');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEventListeners();
         }
     };
